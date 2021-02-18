dbc65d4eda204e66cf60ed8a46537e7957e9baa5
BarnacleCode <kurniawan.rachmad@gmail.com>
2021-02-18 05:45:02 -0500

    commit on 'master'

---
+++ arithmatic.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func arithmatic() {
+	fmt.Println("Testing")
+}
--- print_var.go
+++ print_var.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func printvar() {
 	var str string
 
 	//assign values
